6342e0137aa5efb803bdd8045e1b2e32baf6ed91
attakei <attakei@gmail.com>
2015-11-20 22:01:02 +0900

    css -> scss

--- www/gulpfile.babel.js
+++ www/gulpfile.babel.js
@@ -13,16 +13,24 @@ var config = {
 /*******************
  * src -> dist
  *******************/ 
-gulp.task('build', ['build:copy_vendor', 'build:copy']);
+gulp.task('build', ['build:copy_vendor', 'build:copy', 'compile:scss']);
 
 gulp.task('build:copy', (callback) => {
-  gulp.src(config.path.src+'/**')
+  gulp.src([config.path.src+'/**.html'])
     .pipe(gulp.dest(config.path.dist))
 });
 
 gulp.task('build:copy_vendor', (callback) => {
   gulp.src(config.path.vendor+'/**')
     .pipe(gulp.dest(config.path.dist+'/vendor'))
+});
+
+
+gulp.task('compile:scss', (callback) => {
+  var sass = require('gulp-sass');
+  gulp.src(config.path.src + '/scss/**.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(config.path.dist + '/css'))
 });
 
 
