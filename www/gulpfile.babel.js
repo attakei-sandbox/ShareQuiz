ffd508366d914b3ee7f7866c1eec8f0f3607d05f
attakei <attakei@gmail.com>
2015-11-20 22:15:06 +0900

    中間ファイル置き場としてlibを用意

--- www/gulpfile.babel.js
+++ www/gulpfile.babel.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var config = {
     path: {
         src: './src',
+        out: './lib',
         dist: './dist',
         vendor: './vendor',
     }
@@ -17,12 +18,12 @@ gulp.task('build', ['build:copy_vendor', 'build:copy', 'compile:scss']);
 
 gulp.task('build:copy', (callback) => {
   gulp.src([config.path.src+'/**.html'])
-    .pipe(gulp.dest(config.path.dist))
+    .pipe(gulp.dest(config.path.out))
 });
 
 gulp.task('build:copy_vendor', (callback) => {
   gulp.src(config.path.vendor+'/**')
-    .pipe(gulp.dest(config.path.dist+'/vendor'))
+    .pipe(gulp.dest(config.path.out+'/vendor'))
 });
 
 
@@ -30,14 +31,21 @@ gulp.task('compile:scss', (callback) => {
   var sass = require('gulp-sass');
   gulp.src(config.path.src + '/scss/**.scss')
     .pipe(sass())
-    .pipe(gulp.dest(config.path.dist + '/css'))
+    .pipe(gulp.dest(config.path.out + '/css'))
 });
 
 
 /*******************
  * cleanup
  *******************/ 
-gulp.task('clean', ['clean:dist']);
+gulp.task('clean', ['clean:lib', 'clean:dist']);
+
+gulp.task('clean:lib', (callback) => {
+  let fs = require('fs')
+  let del = require('del')
+  del.sync(config.path.out)
+  fs.mkdirSync(config.path.out)
+});
 
 gulp.task('clean:dist', (callback) => {
   let fs = require('fs')
