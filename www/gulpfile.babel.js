60f8b793e7b9ba62e3b8067cb61d726c6fcc09e2
attakei <attakei@gmail.com>
2015-11-22 02:34:42 +0900

    React 系ライブラリをscriptタグで処理する。jsxをデプロイする想定

--- www/gulpfile.babel.js
+++ www/gulpfile.babel.js
@@ -14,10 +14,15 @@ var config = {
 /*******************
  * src -> dist
  *******************/ 
-gulp.task('build', ['copy:vendor', 'copy:dummy', 'compile:html', 'compile:scss']);
+gulp.task('build', ['copy:vendor', 'copy:dummy', 'build:copy']);
 
 gulp.task('build:copy', (callback) => {
-  gulp.src([config.path.src+'/**/*.html', config.path.src+ '/**/*.css'])
+  var targets = [
+    config.path.src+'/**/*.html',
+    config.path.src+ '/**/*.css',
+    config.path.src+ '/**/*.jsx',
+  ];
+  gulp.src(targets)
     .pipe(gulp.dest(config.path.out))
 });
 
@@ -36,28 +41,6 @@ gulp.task('copy:dummy', (callback) => {
 gulp.task('copy:vendor', (callback) => {
   gulp.src(config.path.vendor+'/**')
     .pipe(gulp.dest(config.path.out+'/vendor'))
-});
-
-
-gulp.task('webpack', function () {
-  var webpack = require('webpack-stream');
-  var webpackConfig = {
-    entry: config.path.src + '/jsx/index.jsx',
-    output: {
-      filename: 'js/index.js'
-    },
-    module: {
-      loaders: [
-         { test: /\.jsx$/, loader: 'jsx-loader' }
-      ]
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    }
-  };
-  gulp.src(config.path.src + '/jsx/*')
-    .pipe(webpack(webpackConfig))
-    .pipe(gulp.dest(config.path.out));
 });
 
 
