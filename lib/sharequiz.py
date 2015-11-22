7d6ea1f0510041348cedd2a1fd1a08e18f68f702
attakei <attakei@gmail.com>
2015-11-23 01:54:51 +0900

    DynamoDBの中身をソートして返す。未来を出さない

--- lib/sharequiz.py
+++ lib/sharequiz.py
@@ -4,21 +4,37 @@
 __version__ = '0.0.1'
 
 
+from datetime import datetime
 import boto3
 
 
 def fetch_current_articles(event, context):
-    '''記事リストを返す
+    """記事リストを返す
 
     :param event:
     :param context:
     :return:
-    '''
+    """
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('sharequiz-dev-articles')
     
-    results = table.scan()
-    # TODO: It is a stub
+    now_string = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    results = table.scan(
+        IndexName='published_date-index',
+        Select='ALL_ATTRIBUTES',
+        Limit=100,
+        ScanFilter={
+            'published_date': {
+                'AttributeValueList': [
+                    now_string
+                ],
+                'ComparisonOperator': 'LT',
+            }
+        },
+    )
+    articles = results.get('Items')
+    articles.sort(key=lambda art: art['published_date'], reverse=True)
+
     return {
-        'articles': results.get('Items')
+        'articles': articles
     }
