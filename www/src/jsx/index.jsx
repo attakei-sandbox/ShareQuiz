d7b319d37241e36ed0b4c19a64548a5b198be9f0
attakei <attakei@gmail.com>
2015-11-22 17:54:38 +0900

    ArticleItemでレンダリングする

--- www/src/jsx/index.jsx
+++ www/src/jsx/index.jsx
@@ -1,4 +1,67 @@
+class ArticleItem extends React.Component
+{
+  render()
+  {
+    var article = this.props.article;
+    var site = this.props.article.site;
+    return(
+      <div>
+        <h4 className="list-group-item-heading">
+          <a href={article.url}>{article.title}</a>&nbsp;
+          <small>from&nbsp;<a href={site.url}>{site.title}</a></small>
+        </h4>
+        <p className="list-group-item-text" style={{marginLeft: '1em'}}>{article.description}</p>
+        <p className="text-right">{article.published_date}</p>
+      </div>
+    );    
+  }  
+}
+
+class App extends React.Component
+{
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [
+        {
+          url: "http://example.com/1",
+          title: "test title 1",
+          description: "test site is now developing 1.",
+          site: {
+            url: "http://example.com/",
+            title: "Example title",
+          }
+        },
+        {
+          url: "http://example.com/2",
+          title: "test title 2",
+          description: "test site is now developing 2.",
+          site: {
+            url: "http://example.com/",
+            title: "Example title",
+          }
+        },
+      ]
+    };
+  }
+
+  render() {
+    var articleItems = this.state.articles.map((article) => {
+      return (
+        <li className="list-group-item">
+          <ArticleItem article={article} />
+        </li>
+      );
+    });
+    return(
+      <ul className="list-group">
+        {articleItems}
+      </ul>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('main')
 );
